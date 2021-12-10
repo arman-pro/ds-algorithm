5569a70b7b506d97f5443ebff97bed8c3982f28b
Arman Ali <mohammadarmanali.din@gmail.com>
2021-12-10 21:31:02 +0600

    create linear search fun

--- algorithm/linear Search/LinearSearch.js
+++ algorithm/linear Search/LinearSearch.js
@@ -1,3 +1,21 @@
-function LinearSearch(arr) {
-  // our code will be here
+/**
+ * Linear Search Algorithm
+ * Create a function that takes two @var arr & @var item
+ * return @var item index from @var arr
+ * @param {Array|String} arr
+ * @param {int|string} item
+ * @returns {int} index of item
+ */
+function linearSearch(arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      return i;
+    }
+  }
+  return -1;
 }
+
+console.log(linearSearch([1, 5, 3, 2, 4, 6], 6)); // 5
+console.log(linearSearch([15, 63, 2, -1], 8)); // -1
+console.log(linearSearch("Arman", "n")); // 4
+console.log(linearSearch([], 4)); // -1
